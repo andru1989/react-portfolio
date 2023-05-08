a46f89ddc3d9ef368c9f9e58b31ad4446fad67a2
Andres Vizcaino <andruvizcaino1989@gmail.com>
2023-05-07 19:04:29 -0500

    feat: add babel and eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,28 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true, amd: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    }
+  },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'no-use-before-define': 'warn',
+    // 'comma-dangle': ['error', 'never'],
+    'indent': ['error', 2],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'no-multi-spaces': ['error']
   },
-}
+};
